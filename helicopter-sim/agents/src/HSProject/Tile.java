cb541a9c40dbe19ad38e84e2da2a6ad544d5795d
Harrison <hs750@york.ac.uk>
2015-10-17 12:11:15 +0100

    tile coded q table

---
+++ helicopter-sim/agents/src/HSProject/Tile.java
@@ -0,0 +1,47 @@
+package HSProject;
+
+import marl.environments.State;
+
+/**
+ * A Tile is very simple implementation of a State
+ * Representation where it is given an unsigned int
+ * value on construction and then simply give that
+ * value as the hash value required by the State
+ * Representation interface.
+ */
+public class Tile
+	implements State<Tile>
+{
+	protected int value_;
+	
+	public Tile()
+	{
+		this(0);
+	}
+	public Tile(int value)
+	{
+		value_ = value;
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return value_;
+	}
+	
+    @Override
+    public void set(Tile s)
+    {
+        value_ = s.value_;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+    	if(obj instanceof Tile){
+    		Tile t = (Tile) obj;
+    		return this.value_ == t.value_;
+    	}
+		return false;
+    }
+	
+}
---
+++ helicopter-sim/agents/src/HSProject/TileCodeQTable.java
@@ -0,0 +1,84 @@
+package HSProject;
+
+import java.util.HashMap;
+
+import org.rlcommunity.rlglue.codec.types.Action;
+
+import HSProject.Tile;
+
+public class TileCodeQTable {
+	HashMap<Tile, HashMap<Tile, ActionValue>> table = new HashMap<Tile, HashMap<Tile, ActionValue>>();
+
+	private static double DEFAULT_Q_VAL = 0;
+	
+	public TileCodeQTable() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public double getQValue(Tile state, Tile action){
+		HashMap<Tile, ActionValue> actionValues = table.get(state);
+		if(actionValues == null){
+			actionValues = new HashMap<Tile, ActionValue>();
+		}
+		ActionValue av = actionValues.get(action);
+		if(av != null){
+			return av.value;
+		}
+		return DEFAULT_Q_VAL;
+	}
+	
+	public double getMaxQValue(Tile state){
+		ActionValue maxAV = getMaxAction(state);
+		if(maxAV.actualAction == null){
+			return DEFAULT_Q_VAL;
+		}
+		return maxAV.value;
+	}
+	
+	public ActionValue getMaxAction(Tile state){
+		HashMap<Tile, ActionValue> actionValues = table.get(state);
+		if(actionValues == null){
+			actionValues = new HashMap<Tile, ActionValue>();
+		}
+		ActionValue maxAV = new ActionValue(-Double.MAX_VALUE, null);;
+		for(ActionValue av : actionValues.values()){
+			if(av.value > maxAV.value){
+				maxAV = av;
+			}
+		}
+		return maxAV;
+	}
+	
+	
+	public void put(Tile state, Tile action, double value, Action actualAction){
+		HashMap<Tile, ActionValue> av = table.get(state);
+		if(av == null){
+			av = new HashMap<Tile, ActionValue>();
+		}
+		av.put(action, new ActionValue(value, actualAction));
+		table.put(state, av);
+	}
+	
+	public class ActionValue{
+		private double value;
+		private Action actualAction;
+		public ActionValue(double value, Action actualAction) {
+			this.value = value;
+			if(actualAction != null){
+				this.actualAction = new Action(actualAction);
+			}else{
+				this.actualAction = null;
+			}
+		}
+		
+		public double getValue() {
+			return value;
+		}
+		
+		public Action getAction() {
+			return actualAction;
+		}
+		
+	}
+	
+}
