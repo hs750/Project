672364621580898b431b0cb5a22a17035764c5c3
Harrison Spain <contact@harrison-spain.com>
2016-01-26 16:35:41 +0000

    run bash hierarchy

--- helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedQ/run.bash
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedQ/run.bash
@@ -1,11 +1,2 @@
-#!/bin/bash
-basePath=../../../../
-
-systemPath=$basePath/system
-#Source a script that sets all important functions and variables
-source $systemPath/rl-competition-includes.sh
-
-className=HSProject.helicopterAgentTileCodedQ.HelicopterAgentTileCodedQ    #Fully Qualified Name of the agent class
-maxMemory=4096M			 #Max amount of memory to give the agent (Java default is often too low)
-extraPath=./../../../bin/HelicopterAgentTileCodedQ:../../../../../YORLL.jar #Item for the class path so your agent can be found
-startJavaAgent $extraPath $className $maxMemory
+cd ..
+bash runTileCoded.bash Q
