673aab971c21f7a854d98c0dd7c3af4536974613
Harrison Spain <contact@harrison-spain.com>
2016-01-26 16:35:06 +0000

    migrated eligibility q table to new hash map

--- helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedEligibilitySARSA/EligibilityQTable.java
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedEligibilitySARSA/EligibilityQTable.java
@@ -1,24 +1,24 @@
 package HSProject.tileCoded.helicopterAgentTileCodedEligibilitySARSA;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 import org.rlcommunity.rlglue.codec.types.Action;
 
-import com.sun.xml.internal.ws.assembler.dev.ServerTubelineAssemblyContext;
-
 import HSProject.tileCoded.tilings.Tile;
 import HSProject.tileCoded.tilings.TileCodeQTable;
+import net.openhft.koloboke.collect.map.hash.HashIntDoubleMaps;
+import net.openhft.koloboke.collect.map.hash.HashIntObjMaps;
 
 public class EligibilityQTable extends TileCodeQTable {
-	HashMap<Tile, HashMap<Tile, Double>> eligibilityTable;
+	Map<Integer, Map<Integer, Double>> eligibilityTable;
 	
 	double alpha;
 	double gamma;
 	double lambda;
 	
 	public EligibilityQTable(double alpha, double gamma, double lambda) {
-		eligibilityTable = new HashMap<Tile, HashMap<Tile,Double>>();
+		eligibilityTable = HashIntObjMaps.<Map<Integer, Double>>newUpdatableMap();
 		
 		this.alpha = alpha;
 		this.gamma = gamma;
@@ -26,14 +26,14 @@ public class EligibilityQTable extends TileCodeQTable {
 	}
 	
 	public void eligibilityUpdate(Tile state, Tile action, double delta, Action lastAction){
-		double oldElig = getEligibilityValue(state, action);
-		putEligibilityValue(state, action, oldElig + 1);
+		double oldElig = getEligibilityValue(state.hashCode(), action.hashCode());
+		putEligibilityValue(state.hashCode(), action.hashCode(), oldElig + 1);
 		
 		double q = getQValue(state, action);
-		put(state, action, q + alpha * delta * getEligibilityValue(state, action), lastAction);
+		put(state, action, q + alpha * delta * getEligibilityValue(state.hashCode(), action.hashCode()), lastAction);
 		
-		for (Entry<Tile, HashMap<Tile, ActionValue>> states : table.entrySet()){
-			for (Entry<Tile, ActionValue> actions : states.getValue().entrySet()){
+		for (Entry<Integer, Map<Integer, ActionValue>> states : table.entrySet()){
+			for (Entry<Integer, ActionValue> actions : states.getValue().entrySet()){
 				double curElig = getEligibilityValue(states.getKey(), actions.getKey());
 				
 				if(!state.equals(states.getKey()) && !action.equals(actions.getKey())){
@@ -48,10 +48,10 @@ public class EligibilityQTable extends TileCodeQTable {
 		
 	}
 	
-	private double getEligibilityValue(Tile state, Tile action){
-		HashMap<Tile, Double> eligActions = eligibilityTable.get(state);
+	private double getEligibilityValue(Integer state, Integer action){
+		Map<Integer, Double> eligActions = eligibilityTable.get(state);
 		if(eligActions == null){
-			eligActions = new HashMap<Tile, Double>();
+			return 0;
 		}
 		Double oldElig = eligActions.get(action);
 		if(oldElig == null){
@@ -60,10 +60,10 @@ public class EligibilityQTable extends TileCodeQTable {
 		return oldElig;
 	}
 	
-	private void putEligibilityValue(Tile state, Tile action, double elig){
-		HashMap<Tile, Double> eligActions = eligibilityTable.get(state);
+	private void putEligibilityValue(Integer state, Integer action, double elig){
+		Map<Integer, Double> eligActions = eligibilityTable.get(state);
 		if(eligActions == null){
-			eligActions = new HashMap<Tile, Double>();
+			eligActions = HashIntDoubleMaps.newUpdatableMap();
 		}
 		eligActions.put(action, elig);
 		eligibilityTable.put(state, eligActions);
