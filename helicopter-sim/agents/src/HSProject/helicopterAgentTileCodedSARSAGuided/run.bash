aad1e89390992c0ab9f51194c6472f186ce3c677
Harrison Spain <contact@harrison-spain.com>
2016-01-26 10:14:24 +0000

    make and run files for guided

--- helicopter-sim/agents/src/HSProject/helicopterAgentTileCodedSARSAGuided/run.bash
+++ helicopter-sim/agents/src/HSProject/helicopterAgentTileCodedSARSAGuided/run.bash
@@ -5,6 +5,6 @@ systemPath=$basePath/system
 source $systemPath/rl-competition-includes.sh
 
 className=HSProject.helicopterAgentTileCodedSARSAGuided.HelicopterAgentTileCodedSARSAGuided  #Fully Qualified Name of the agent class
-maxMemory=4098M			 #Max amount of memory to give the agent (Java default is often too low)
-extraPath=./../../../bin/HelicopterAgentTileCodedSARSAGuided:../../../../../YORLL.jar		 #Item for the class path so your agent can be found
+maxMemory=6144M			 #Max amount of memory to give the agent (Java default is often too low)
+extraPath=./../../../bin/HelicopterAgentTileCodedSARSAGuided:../../../../../YORLL.jar:../../../../../koloboke-api-jdk8-0.6.8.jar:../../../../../koloboke-impl-jdk8-0.6.8.jar		 #Item for the class path so your agent can be found
 startJavaAgent $extraPath $className $maxMemory
