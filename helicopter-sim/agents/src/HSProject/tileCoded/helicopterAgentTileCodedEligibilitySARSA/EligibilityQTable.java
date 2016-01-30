fd9700dab4435794a2f37ebbf138aaf1de9e9b41
Harrison Spain <contact@harrison-spain.com>
2016-01-30 18:30:39 +0000

    putting optimisation

--- helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedEligibilitySARSA/EligibilityQTable.java
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedEligibilitySARSA/EligibilityQTable.java
@@ -9,47 +9,49 @@ import net.openhft.koloboke.collect.map.hash.HashIntObjMaps;
 
 public class EligibilityQTable extends TileCodeQTable {
 	Map<Integer, Map<Integer, Double>> eligibilityTable;
-	
+
 	double alpha;
 	double gamma;
 	double lambda;
-	
+
 	public EligibilityQTable(double alpha, double gamma, double lambda) {
 		this.alpha = alpha;
 		this.gamma = gamma;
 		this.lambda = lambda;
-		
-		eligibilityTable = HashIntObjMaps.<Map<Integer, Double>>newUpdatableMap();
+
+		eligibilityTable = HashIntObjMaps.<Map<Integer, Double>> newUpdatableMap();
 	}
-	
-	private double getEligibilityValue(Integer state, Integer action){
+
+	private double getEligibilityValue(Integer state, Integer action) {
 		Map<Integer, Double> eligActions = eligibilityTable.get(state);
-		if(eligActions == null){
+		if (eligActions == null) {
 			return 0;
 		}
 		Double oldElig = eligActions.get(action);
-		if(oldElig == null){
+		if (oldElig == null) {
 			return 0;
 		}
 		return oldElig;
 	}
-	
-	private void putEligibilityValue(Integer state, Integer action, double elig){
+
+	private void putEligibilityValue(Integer state, Integer action, double elig) {
 		Map<Integer, Double> eligActions = eligibilityTable.get(state);
-		if(eligActions == null){
+		boolean nullEligibility = eligActions == null;
+		if (nullEligibility) {
 			eligActions = HashIntDoubleMaps.newUpdatableMap();
 		}
 		eligActions.put(action, elig);
-		eligibilityTable.put(state, eligActions);
+		if (nullEligibility) {
+			eligibilityTable.put(state, eligActions);
+		}
 	}
-	
-	public void updateEligibility(Tile state, Tile action, double eligibility){
+
+	public void updateEligibility(Tile state, Tile action, double eligibility) {
 		putEligibilityValue(state.hashCode(), action.hashCode(), eligibility);
 	}
-	
-	public double getEligibility(Tile state, Tile action){
+
+	public double getEligibility(Tile state, Tile action) {
 		return getEligibilityValue(state.hashCode(), action.hashCode());
 	}
-	
-	
+
 }
