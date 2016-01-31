bbb58bf234f989c4139f8b20a5793d14f95c1ff8
Harrison Spain <contact@harrison-spain.com>
2016-01-31 15:36:25 +0000

    9 state

---
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedSARSA9State/HelicopterAgentTileCodedSARSA9State.java
@@ -0,0 +1,51 @@
+package HSProject.tileCoded.helicopterAgentTileCodedSARSA9State;
+
+
+
+import org.rlcommunity.rlglue.codec.types.Observation;
+import org.rlcommunity.rlglue.codec.util.AgentLoader;
+
+import HSProject.tileCoded.TileCodedAgentSARSA;
+import HSProject.tileCoded.tilings.StateManipulator;
+import HSProject.tileCoded.tilings.StateManipulator9;
+
+public class HelicopterAgentTileCodedSARSA9State extends TileCodedAgentSARSA {
+	private static double alpha = 0.1;
+	private static double gamma = 1;
+	private StateManipulator sm;
+	
+	public HelicopterAgentTileCodedSARSA9State() {
+		super(alpha, gamma);
+		
+		
+		int numTiles = 10;
+		int numVariables = 9;
+		int numTilings = 16;
+		double[] statesMin = {-5, -5, -5, -20, -20, -20, -12.566, -12.566, -12.566};
+		double[] statesMax = {5, 5, 5, 20, 20, 20, 12.566, 12.566, 12.566};
+		
+		initialiseStateTiling(numVariables, statesMin, statesMax, numTiles, numTilings);
+
+		
+		numTiles = 5;
+		numVariables = 4;
+		numTilings = 16;
+		double[] actionsMin = {-1, -1, -1, -1};
+		double[] actionsMax = {1, 1, 1, 1};
+
+		initialiseActionTiling(numVariables, actionsMin, actionsMax, numTiles, numTilings);
+		
+		sm = new StateManipulator9();
+	}
+	
+	@Override
+	protected Observation manipulateState(Observation o){
+		return sm.manipulateState(o);
+	}
+
+	public static void main(String[] args) {
+		AgentLoader L = new AgentLoader(new HelicopterAgentTileCodedSARSA9State());
+		L.run();
+	}
+
+}
---
+++ helicopter-sim/agents/src/HSProject/tileCoded/tilings/StateManipulator9.java
@@ -0,0 +1,30 @@
+package HSProject.tileCoded.tilings;
+
+import org.rlcommunity.rlglue.codec.types.Observation;
+
+/**
+ * R. Koppejan and S. Whiteson, “Neuroevolutionary reinforcement learning for generalized control of simulated helicopters.,” Evol. Intell., vol. 4, no. 4, pp. 219–241, 2011.
+ * @author harrison
+ *
+ */
+public class StateManipulator9 implements StateManipulator {
+
+	@Override
+	public Observation manipulateState(Observation o) {
+		Observation newO = new Observation(0, 9);
+		//Positions
+		newO.doubleArray[0] = o.doubleArray[0];
+		newO.doubleArray[1] = o.doubleArray[1];
+		newO.doubleArray[2] = o.doubleArray[2];
+		//Angles
+		newO.doubleArray[3] = o.doubleArray[3];
+		newO.doubleArray[4] = o.doubleArray[4];
+		newO.doubleArray[5] = o.doubleArray[5];
+		//Velocities
+		newO.doubleArray[6] = o.doubleArray[6];
+		newO.doubleArray[7] = o.doubleArray[7];
+		newO.doubleArray[8] = o.doubleArray[8];
+		return newO;
+	}
+
+}
