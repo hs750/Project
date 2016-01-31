44ecb90649e5bc5d6fa19ae25413c5f7f4e20ca0
Harrison Spain <contact@harrison-spain.com>
2016-01-31 21:32:05 +0000

    fixed equals methods and added javadoc

--- helicopter-sim/agents/src/HSProject/tileCoded/tilings/TileCodedHelicopterAction.java
+++ helicopter-sim/agents/src/HSProject/tileCoded/tilings/TileCodedHelicopterAction.java
@@ -1,28 +1,45 @@
 package HSProject.tileCoded.tilings;
 
+import java.util.Arrays;
+
 import org.rlcommunity.rlglue.codec.types.Action;
 import org.rlcommunity.rlglue.codec.types.RL_abstract_type;
 
-public class TileCodedHelicopterAction implements marl.ext.tilecoding.TileCodingState<TileCodedHelicopterAction>{
+/**
+ * Action representation for input into tile coding algorithm. Acts as a bridge
+ * between RL-Glue and YORLL.
+ * 
+ * @author harrison
+ *
+ */
+public class TileCodedHelicopterAction implements marl.ext.tilecoding.TileCodingState<TileCodedHelicopterAction> {
 	private Action a;
-	
-	public TileCodedHelicopterAction(Action o) {
-		this.a = new Action(o);
-	}
-	
-	public void set(TileCodedHelicopterAction state) {
-		RL_abstract_type.RLStructCopy(state.a, this.a);
+
+	/**
+	 * A new Action
+	 * 
+	 * @param a
+	 *            the action
+	 */
+	public TileCodedHelicopterAction(Action a) {
+		this.a = new Action(a);
 	}
 
+	@Override
+	public void set(TileCodedHelicopterAction action) {
+		RL_abstract_type.RLStructCopy(action.a, this.a);
+	}
+
+	@Override
 	public double getFeature(int i) {
 		return a.doubleArray[i];
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		if(obj instanceof TileCodedHelicopterAction){
+		if (obj instanceof TileCodedHelicopterAction) {
 			TileCodedHelicopterAction tchs = (TileCodedHelicopterAction) obj;
-			return this.a.doubleArray == tchs.a.doubleArray;
+			return Arrays.equals(this.a.doubleArray, tchs.a.doubleArray);
 		}
 		return false;
 	}
