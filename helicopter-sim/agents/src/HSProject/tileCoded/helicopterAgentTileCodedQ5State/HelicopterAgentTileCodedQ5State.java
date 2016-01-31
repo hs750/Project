69d01367092f9b00470b1823039d0cdb0f278b1c
Harrison Spain <contact@harrison-spain.com>
2016-01-31 16:02:11 +0000

    5 state

---
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedQ5State/HelicopterAgentTileCodedQ5State.java
@@ -0,0 +1,51 @@
+package HSProject.tileCoded.helicopterAgentTileCodedQ5State;
+
+
+
+import org.rlcommunity.rlglue.codec.types.Observation;
+import org.rlcommunity.rlglue.codec.util.AgentLoader;
+
+import HSProject.tileCoded.TileCodedAgentQ;
+import HSProject.tileCoded.tilings.StateManipulator;
+import HSProject.tileCoded.tilings.StateManipulator5;
+
+public class HelicopterAgentTileCodedQ5State extends TileCodedAgentQ {
+	private static double alpha = 0.1;
+	private static double gamma = 1;
+	private StateManipulator sm;
+	
+	public HelicopterAgentTileCodedQ5State() {
+		super(alpha, gamma);
+		
+		
+		int numTiles = 10;
+		int numVariables = 5;
+		int numTilings = 16;
+		double[] statesMin = {-5, -5, -5, -1, -1};
+		double[] statesMax = {5, 5, 5, 1, 1};
+		
+		initialiseStateTiling(numVariables, statesMin, statesMax, numTiles, numTilings);
+
+		
+		numTiles = 5;
+		numVariables = 4;
+		numTilings = 16;
+		double[] actionsMin = {-1, -1, -1, -1};
+		double[] actionsMax = {1, 1, 1, 1};
+
+		initialiseActionTiling(numVariables, actionsMin, actionsMax, numTiles, numTilings);
+		
+		sm = new StateManipulator5();
+	}
+	
+	@Override
+	protected Observation manipulateState(Observation o){
+		return sm.manipulateState(o);
+	}
+
+	public static void main(String[] args) {
+		AgentLoader L = new AgentLoader(new HelicopterAgentTileCodedQ5State());
+		L.run();
+	}
+
+}
---
+++ helicopter-sim/agents/src/HSProject/tileCoded/tilings/StateManipulator5.java
@@ -0,0 +1,33 @@
+package HSProject.tileCoded.tilings;
+
+import org.rlcommunity.rlglue.codec.types.Observation;
+
+/**
+ * The states used by both <br>
+ * A. Y. Ng, H. J. Kim, M. I. Jordan, and S. Sastry, “Autonomous helicopter
+ * flight via Reinforcement Learning,” Adv. Neural Inf. Process. Syst. 16, vol.
+ * 16, pp. 363–372, 2004. <br>
+ * and <br>
+ * R. Koppejan and S. Whiteson, “Neuroevolutionary reinforcement learning for
+ * generalized control of simulated helicopters.,” Evol. Intell., vol. 4, no. 4,
+ * pp. 219–241, 2011.
+ * 
+ * @author harrison
+ *
+ */
+public class StateManipulator5 implements StateManipulator {
+
+	@Override
+	public Observation manipulateState(Observation o) {
+		Observation newO = new Observation(0, 5);
+		// Velocities
+		newO.doubleArray[0] = o.doubleArray[0];
+		newO.doubleArray[1] = o.doubleArray[1];
+		newO.doubleArray[2] = o.doubleArray[2];
+		// Angles
+		newO.doubleArray[3] = o.doubleArray[9];
+		newO.doubleArray[4] = o.doubleArray[10];
+		return newO;
+	}
+
+}
