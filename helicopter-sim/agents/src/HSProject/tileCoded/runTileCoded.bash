b055ccd60728bfef7117ac8eb525bbd611c2319f
Harrison Spain <contact@harrison-spain.com>
2016-02-04 12:04:32 +0000

    special case for csteach0, cant allocate more than 12GB of RAM

--- helicopter-sim/agents/src/HSProject/tileCoded/runTileCoded.bash
+++ helicopter-sim/agents/src/HSProject/tileCoded/runTileCoded.bash
@@ -17,11 +17,17 @@ maxMemory=4G
 # For Mac OSX use 6 gigabytes, for linux use as much is avaiable
 if [ "$OSTYPE" = "linux-gnu" ]; then #osx is "darwin15"
 
+
+
 # Find the memory avaiable on the system to use for the JVM, round down then subtract 1. eg. 8GB total will be found as 7.somehtingGB rounded down to 7 then minus 1 to give 6.
 maxMemory=$(echo -e 'import re\nmatched=re.search(r"^MemTotal:\s+(\d+)",open("/proc/meminfo").read())\nprint(int(int(matched.groups()[0])/(1024.**2))) - 1' | python)G
 
 else
 maxMemory=6G
+fi
+
+if [ "$HOSTNAME" = "csteach0" ]; then
+maxMemory=12G
 fi
 
 echo "JVM MEMORY: $maxMemory"
