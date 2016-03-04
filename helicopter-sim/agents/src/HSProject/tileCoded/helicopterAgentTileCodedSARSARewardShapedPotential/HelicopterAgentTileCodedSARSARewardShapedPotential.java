194da6db6a9a8b5e8fe52c28e6bce48ca8f5ebe1
Harrison Spain <contact@harrison-spain.com>
2016-03-04 17:03:43 +0000

    configurable reward shaping + potential based reward shaping

---
+++ helicopter-sim/agents/src/HSProject/tileCoded/helicopterAgentTileCodedSARSARewardShapedPotential/HelicopterAgentTileCodedSARSARewardShapedPotential.java
@@ -0,0 +1,104 @@
+package HSProject.tileCoded.helicopterAgentTileCodedSARSARewardShapedPotential;
+
+
+
+import org.rlcommunity.rlglue.codec.types.Action;
+import org.rlcommunity.rlglue.codec.types.Observation;
+import org.rlcommunity.rlglue.codec.util.AgentLoader;
+
+import HSProject.tileCoded.TileCodedAgentSARSA;
+
+/**
+ * An experiment implementing SARSA. The reward received from the
+ * environment is shapped giving better rewards for producing rewards close to
+ * {@link #agent_policy(Observation, Action)}
+ * 
+ * @author harrison
+ *
+ */
+public class HelicopterAgentTileCodedSARSARewardShapedPotential extends TileCodedAgentSARSA {
+	private boolean usePotential;
+	private int potentialFunction;
+	
+	public HelicopterAgentTileCodedSARSARewardShapedPotential() {
+		
+		
+		int numTiles = getConfig().getInt("stateTiles");
+		int numVariables = 12;
+		int numTilings = getConfig().getInt("stateTilings");
+		double[] statesMin = { -5, -5, -5, -20, -20, -20, -12.566, -12.566, -12.566, -1, -1, -1 };
+		double[] statesMax = { 5, 5, 5, 20, 20, 20, 12.566, 12.566, 12.566, 1, 1, 1 };
+
+		initialiseStateTiling(numVariables, statesMin, statesMax, numTiles, numTilings);
+
+		numTiles = getConfig().getInt("actionTiles");
+		numVariables = 4;
+		numTilings = getConfig().getInt("actionTilings");
+		double[] actionsMin = {-1, -1, -1, -1};
+		double[] actionsMax = {1, 1, 1, 1};
+
+		initialiseActionTiling(numVariables, actionsMin, actionsMax, numTiles, numTilings);
+		
+		usePotential = getConfig().getBoolean("usePotential");
+		System.out.println("Potential Reward Shaping=" + usePotential);
+		potentialFunction = getConfig().getInt("potentialFunction");
+		System.out.println("Shaping Function=" + potentialFunction);
+	}
+	
+	/**
+	 * Shape the reward before passing it to the learning process.
+	 * 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Action agent_step(double reward, Observation o) {
+		return super.agent_step(getReward(o, reward), o);
+	}
+	
+	/**
+	 * Potential based reward shaping
+	 * @param o
+	 * @param reward
+	 * @return
+	 */
+	private double getReward(Observation o, double reward){
+		if(usePotential){
+			Observation lastState = getLastState();
+			double r = getR(lastState);
+			double r2 = getR(o);
+			
+			double F = getGamma() * r2 - r;
+			return reward + F;
+		}else{
+			return reward + getR(o);
+		}
+		
+		
+	}
+	
+	/**
+	 * The real-valued funciton of state
+	 * @param s
+	 * @return
+	 */
+	private double getR(Observation s){
+		double r = 0;
+		switch(potentialFunction){
+		case 0:
+		default:
+			r -= s.getDouble(3) * s.getDouble(0);
+			r -= s.getDouble(4) * s.getDouble(1);
+			r -= s.getDouble(5) * s.getDouble(2);
+			break;
+		}
+		
+		
+		return r;
+	}
+
+	public static void main(String[] args) {
+		AgentLoader L = new AgentLoader(new HelicopterAgentTileCodedSARSARewardShapedPotential());
+		L.run();
+	}
+
+}
